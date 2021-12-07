d3ddf4862ea18b8903f9bc91675d7cc7d418c9b9
Corey DeLoach <cdeloach16@gmail.com>
2021-12-07 10:55:27 -0600

    removed scrolltoTop function on page load due to typescript error

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -8,13 +8,13 @@ import News from "./views/News";
 import Resources from "./views/Resources";
 import Contact from "./views/Contact";
 import NavBar from "../components/NavBar";
-import ScrollToTop from "../components/ScrollToTop";
+// import ScrollToTop from "../components/ScrollToTop";
 
 /* HOOK REACT EXAMPLE */
 const App = () => {
   return (
     <BrowserRouter>
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
+// const ScrollToTop = () => {
+//   const { pathname }: any = useLocation();
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+//   return null;
+// };
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
-export default ScrollToTop;
+// export default ScrollToTop;
